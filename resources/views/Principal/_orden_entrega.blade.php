5682c0a8af05dd9cdaaf339850f03e89d56eeff5
Vladimir Farrera <vfv9312@gmail.com>
2024-03-06 20:47:49 -0600

    Actualizar estilos de enlaces en vistas de órdenes

--- resources/views/Principal/_orden_entrega.blade.php
+++ resources/views/Principal/_orden_entrega.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('orden_entrega.index') }}">
     <div
-        class="bg-red-500 text-white rounded-2xl text-center mb-7 xl:mt-0 px-5 xl:p-56  hover:bg-blue-500 cursor-pointer hover:shadow-2xl transition duration-200  animate__animated animate__shakeY">
+        class="flex h-96 flex-col justify-center bg-green-600 text-gray-800 rounded-2xl text-center mb-7 xl:mt-0 px-5 hover:bg-green-700 cursor-pointer hover:shadow-2xl transition duration-200  animate__animated animate__shakeY">
         <i class=" text-8xl fas fa-people-carry"></i>
         <h1 class="text-xl">Orden de Entrega </h1>
     </div>
--- resources/views/Principal/_orden_servicio.blade.php
+++ resources/views/Principal/_orden_servicio.blade.php
@@ -1,6 +1,7 @@
 <a href="{{ route('orden_servicio.index') }}">
     <div
-        class=" flex flex-col justify-center bg-green-500 rounded-2xl text-white text-center mb-7 xl:mt-0  px-5 xl:p-56   hover:bg-blue-500 cursor-pointer hover:shadow-2xl transition duration-200  animate__animated animate__shakeY">
-        <i class=" text-8xl fas fa-motorcycle"></i>
-        <h1 class="text-xl ">Orden de Servicio </h1>
+        class="flex h-96 flex-col justify-center bg-green-600 rounded-2xl text-gray-800 text-center mb-7 xl:mt-0 px-5  hover:bg-green-700 cursor-pointer hover:shadow-2xl transition duration-200 animate__animated animate__shakeY">
+        <i class="text-8xl fas fa-motorcycle"></i>
+        <h1 class="text-xl">Orden de Servicio</h1>
     </div>
+</a>
