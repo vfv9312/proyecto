270832a3c25d8bd9399337a7fd3ca27ba0e80a86
Vladimir Farrera <vfv9312@gmail.com>
2024-07-04 14:13:02 -0600

    Actualizar enlace de Font Awesome en la vista edit.blade.php

--- resources/views/empleados/edit.blade.php
+++ resources/views/empleados/edit.blade.php
@@ -30,6 +30,8 @@
     <link rel="stylesheet" href="/css/admin_custom.css">
     <!-- Tailwind -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!--Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 @endpush
 
 @push('js')
